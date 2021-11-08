79ab787ad4a18c1d79fc005ca0ff8da70a9323d4
eliyanab <eliyanapearl@gmail.com>
2021-11-08 12:24:22 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -8,20 +8,25 @@ $('.hot').each(function () {
   $(this).addClass('cool');
 });
 // traverse the elements
-$('#one').next().next().text("milk");
+$('#one').next().next().text('milk');
 // add a new element by clicking the plus sign
-$('#todo').append("<li>candy</li>");
+$('#todo').append('<li>candy</li>');
 $('#add').click(addElement);
 // before and after are for siblings
 // append and prepend are for parent
 
 function addElement() {
   // add a new element
-  $('#todo').append('<li> </li>')
+    //$('#todo').append('<li> </li>')
   // add a input text box
-  
+  $('#todo').append('<li><input type="text" id="newE"></li');
   // whenever the user are done add the element
-
+  $('#newE').blur(function(){
+    $(this).parent().addClass('cool');
+    var items = $(this).val();
+    // replace input box by the text
+    $(this).parent().text(items);
+  });
 }
 
 // bind click with the event handler
@@ -32,12 +37,10 @@ function changeStyle() {
   if ($(this).hasClass('cool')) {
     $(this).removeClass('cool');
     $(this).addClass('complete');
-  }
-  else if($(this).hasClass('complete')){
+  } else if ($(this).hasClass('complete')) {
     $(this).removeClass('complete');
-    $(this).addClass('cool')
-  }
-  else{
+    $(this).addClass('cool');
+  } else {
     $(this).addClass('complete');
   }
 }
