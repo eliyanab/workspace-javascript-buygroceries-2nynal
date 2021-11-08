daed664d2414e48e39b6584f7e109a11f51d2e00
eliyanab <eliyanapearl@gmail.com>
2021-11-08 13:08:22 -0500

    connect to database

--- index.js
+++ index.js
@@ -17,14 +17,13 @@ import * as firebaseui from "firebaseui";
 
 // Add Firebase project configuration object here
 var firebaseConfig = {
-  apiKey: "AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM",
-  authDomain: "grocerylist-91956.firebaseapp.com",
-  databaseURL: "https://grocerylist-91956.firebaseio.com",
-  projectId: "grocerylist-91956",
-  storageBucket: "grocerylist-91956.appspot.com",
-  messagingSenderId: "813812426276",
-  appId: "1:813812426276:web:93e5897af12892ff78dab1",
-  measurementId: "G-VZ83BTR72T"
+  apiKey: "AIzaSyALeGW44MrQJOQhrSNS2ANWpBMJ1jj6JCk",
+  authDomain: "csci225-eb.firebaseapp.com",
+  projectId: "csci225-eb",
+  storageBucket: "csci225-eb.appspot.com",
+  messagingSenderId: "599518973937",
+  appId: "1:599518973937:web:2aeb284889f7d5c543be50",
+  measurementId: "G-DM3PB5G0PF"
 };
 
 firebase.initializeApp(firebaseConfig);
